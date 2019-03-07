ae5e0e71c40094a27b6363bf877d8619e63dd07b
程高伟 <chenggaowei@gridsum.com>
2019-03-07 09:58:36 +0800

    删除没用的页面

--- web/js/demo/datatables-demo.js
+++ web/js/demo/datatables-demo.js
@@ -1,6 +1,34 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function () {
 
+  addChieseAsc();
+  //添加中文排序
+  function addChieseAsc() {
+    jQuery.fn.dataTableExt.oSort['chinese-asc'] = function (x, y) {
+      x = (x instanceof Array) ? x[0] : x == '-' ? 'z' : x; //z的ASCII码值最大
+      y = (y instanceof Array) ? y[0] : y == '-' ? 'z' : y;
+      //javascript自带的中文比较函数，具体用法可自行查阅了解
+      return x.localeCompare(y);
+    };
+
+    jQuery.fn.dataTableExt.oSort['chinese-desc'] = function (x, y) {
+      x = (x instanceof Array) ? x[0] : x == '-' ? 'z' : x;
+      y = (y instanceof Array) ? y[0] : y == '-' ? 'z' : y;
+      return y.localeCompare(x);
+    };
+
+    // aTypes是插件存放表格内容类型的数组
+    // reg赋值的正则表达式，用来判断是否是中文字符
+    // 返回值push到aTypes数组，排序时扫描该数组，'chinese'则调用上面两个方法。返回null默认是'string'
+    jQuery.fn.dataTableExt.aTypes.push(function (sData) {
+      var reg = /^[\u4e00-\u9fa5]{0,}$/;
+      if (reg.test(sData)) {
+        return 'chinese';
+      }
+      return null;
+    });
+  }
+
   $('#dataTable').DataTable({
     language: {
       "sProcessing": "处理中...",
@@ -26,6 +54,10 @@ $(document).ready(function () {
         "sSortDescending": ": 以降序排列此列"
       }
     },
+    "aoColumns": [
+      { "mDataProp": "department.name", "sType": "chinese" },
+      { "mDataProp": "nickname", "sType": "chinese" }
+    ],
     "serverSide": false,
     ajax: {
       url: window.config.api + '/user/getAllUser',
