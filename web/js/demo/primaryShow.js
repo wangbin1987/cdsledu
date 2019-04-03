7b7fda453c698794ed19665d13656bea8c0e6090
程高伟 <chenggaowei@gridsum.com>
2019-04-03 15:35:56 +0800

    优化幼儿园报名信息展示页

--- web/js/demo/primaryShow.js
+++ web/js/demo/primaryShow.js
@@ -39,25 +39,17 @@ $(document).ready(function () {
             {"data": "type"},
             {"data": "district"},
             {"data": "extra.septemberAdd"},
-            {"data": "extra.currentEnrollment"}
+            {"data": "extra.currentEnrollment"},
+            {"data": "extra.left"}
         ], "columnDefs": [{
             // 定义操作列,######以下是重点########
             "targets": 7,//操作按钮目标列
             "data": null,
             "render": function (data, type, row) {
-                let html = "<a href='javascript:void(0);' onclick='edit(" + row.id + ")' class='view btn btn-default btn-xs'  ><i class='fa fa-edit '></i> 报名</a>";
+                let html = "<a href='javascript:void(0);' onclick='edit(" + row.id + ")' class='view btn btn-default btn-xs'  ><i class='fa fa-check-square '></i> 报名</a>";
                 return html;
             }
-        },
-            {
-                // 定义操作列,######以下是重点########
-                "targets": 6,//操作按钮目标列
-                "data": null,
-                "render": function (data, type, row) {
-                    let html = row.extra.septemberAdd - row.extra.currentEnrollment;
-                    return html;
-                }
-            }],
+        }],
     });
 });
 
