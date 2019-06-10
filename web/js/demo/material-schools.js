e2381407d275d2a130fccd636a522ccc2907d78a
gwcheng <peer44@qq.com>
2019-06-11 07:54:33 +0800

    材料列表

--- web/js/demo/material-schools.js
+++ web/js/demo/material-schools.js
@@ -28,10 +28,27 @@ $(document).ready(function () {
                 "sSortDescending": ": 以降序排列此列"
             }
         },
-        "serverSide": false,
+        "bStateSave": true,//保存状态 进入详情后回退仍然在之前页码
+        "serverSide": true,//服务器端获取数据
         ajax: {
             url: window.config.api + '/material/getApproveSchool',
-            type: 'GET'
+            type: 'POST',
+            data: function (data) {
+                data.approveStatus = $("#approveStatus").val();
+                return JSON.stringify(data)
+            },
+            error: function (xhr) {
+                toastr.warning(xhr.responseJSON.message);
+            },
+            "dataFilter": function (json) { // json是服务器端返回的数据
+                json = JSON.parse(json);
+                let returnData = {};
+                returnData.draw = json.data.draw;
+                returnData.recordsTotal = json.data.total;// 返回数据全部记录
+                returnData.recordsFiltered = json.data.total;// 后台不实现过滤功能，每次查询均视作全部结果
+                returnData.data = json.data.list;// 返回的数据列表
+                return JSON.stringify(returnData);// 这几个参数都是datatable需要的，必须要
+            }
         },
         "aaSorting": [[3, "asc"]],
         "columns": [
@@ -63,4 +80,8 @@ $(document).ready(function () {
 function approve(id) {
     window.location = "./school-files.html?schoolCode=" + id;
 }
+
+$("#approveStatus").change(function () {
+    $('#dataTable').DataTable().ajax.reload();
+})
 
